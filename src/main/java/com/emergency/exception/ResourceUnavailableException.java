978381fe4a3df2e884b8bd8a455dcf9e676f98cf
nandanaj2006 <nandanaj2103@gmail.com>
2026-09-10 15:29:58 +0530

    Create ResourceUnavailableException.java

---
+++ src/main/java/com/emergency/exception/ResourceUnavailableException.java
@@ -0,0 +1,7 @@
+package com.emergency.exception;
+
+public class ResourceUnavailableException extends Exception {
+    public ResourceUnavailableException(String message) {
+        super(message);
+    }
+}
